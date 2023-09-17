a903e337ad895a4b3706ab3449e9a9115cf864cf
Frederic Wilhelm <frederic.wilhelm@gmx.net>
2023-09-17 20:54:34 +0200

    Add application

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/fwilhelm92/gha-demo/lib"
+)
+
+type MissingInput struct {
+	input string
+}
+
+func (m *MissingInput) Error() string {
+	return fmt.Sprintf("fehlender input '%v'", m.input)
+}
+
+func main() {
+	if err := doMain(); err != nil {
+		log.Fatalf("gha-demo failed: %v", err)
+	}
+}
+
+func doMain() error {
+
+	input1 := flag.String("zahl1", "", "1. Zahl")
+	input2 := flag.String("zahl2", "", "2. Zahl")
+	operator := flag.String("operator", "", "Operator (+)")
+
+	flag.Parse()
+
+	// Validation
+	if *input1 == "" {
+		return &MissingInput{"zahl1"}
+	}
+	zahl1, err := strconv.Atoi(*input1)
+	if err != nil {
+		return err
+	}
+
+	if *input2 == "" {
+		return &MissingInput{"zahl2"}
+	}
+	zahl2, err := strconv.Atoi(*input2)
+	if err != nil {
+		return err
+	}
+
+	allowedOperator := false
+	for _, o := range [3]string{"+"} {
+		if o == *operator {
+			allowedOperator = true
+		}
+	}
+	if !allowedOperator {
+		return fmt.Errorf("operator ist nicht erlaubt: %v", *operator)
+	}
+
+	var result int
+	switch *operator {
+	case "+":
+		result = lib.Sum(zahl1, zahl2)
+	default:
+		return fmt.Errorf("operator ist nicht implementiert: %v", *operator)
+	}
+
+	fmt.Printf("%v %v %v = %v\n", zahl1, *operator, zahl2, result)
+
+	return nil
+}
